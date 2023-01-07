21ba26f073ddbffe2b91736d9c112e24d2043b7d
Kaung Htun Thant <3199busi@gmail.com>
2023-01-07 23:59:51 +0630

    Splited backend file to function and trigger, perfected empCreate and added empRead function

--- backend.php
+++ backend.php
@@ -1,4 +1,6 @@
 <?php
+	session_start();
+
 	if(isset($_POST["empCreate"])){
 		$url = 'http://localhost/kbtc_oid_server/backend.php';
 		$empID = htmlspecialchars($_POST['empID']);
@@ -7,10 +9,17 @@
 		$empPositionID = htmlspecialchars($_POST['empPositionID']);
 		$empDeptID = htmlspecialchars($_POST['empDeptID']);
 		$empJoinDate = htmlspecialchars($_POST['empJoinDate']);
-		$empKey = htmlspecialchars($_POST['empKey']);
-		$empStatus = 1;
 
-		$data = array('empCreate' => True, 'empID' => $empID, 'empName' => $empName);
+		$data = array(
+			'url' => $url,
+			'empCreate' => True, 
+			'empID' => $empID, 
+			'empName' => $empName,
+			'empNRC' => $empNRC,
+			'empPositionID' => $empPositionID,
+			'empDeptID' => $empDeptID,
+			'empJoinDate' => $empJoinDate,
+		);
 
 		// use key 'http' even if you send the request to https://...
 		$options = array(
@@ -22,8 +31,15 @@
 		);
 		$context  = stream_context_create($options);
 		$result = file_get_contents($url, false, $context);
-		if ($result === FALSE) { /* Handle error */ }
+		if ($result === FALSE) {
+			$_SESSION['Message'] = $json->Message;
+		}
+		$json = json_decode($result);
+		// echo $json->Message;
+		$_SESSION['Message'] = $json->Message;
 
-		var_dump($result);
+		header("Location:".$_SERVER['HTTP_REFERER']);
 	}
+
+	
 ?>
--- emprecs.php
+++ emprecs.php
@@ -1,3 +1,6 @@
+<?php
+	session_start();
+?>
 <!DOCTYPE html>
 <html>
 	<?php include "includes/head.php"; ?>
@@ -13,51 +16,62 @@
 						<li class="breadcrumb-item active" aria-current="page">Employee Records</li>
 					</ol>
 				</nav>
+				<div class="container">
+			  		<?php
+			  			if (isset($_SESSION['Message'])) {
+			  				echo $_SESSION['Message'];
+			  				unset($_SESSION['Message']);
+			  			}
+			  		?>
+			  	</div>
 		  		<div class="row">
-		  			<div class="col container p-5 mx-5 card">
-		  				<form action="backend.php" class="row g-3 needs-validation" novalidate method="GET">
+		  			<div class="col-md-11 container p-5 mx-5 card">
+		  				<form action="backend.php" class="row g-3 needs-validation" method="POST">
 		  					<h3>New Employee Form</h3>
 		  					<hr>
 		  					<input type="hidden" name="userID" value="<?php echo "sessionID"; ?>">
 		  					<input type="hidden" name="category" value="employee">
 							<div class="col-md-4">
 								<div class="form-outline">
-								  	<input type="text" class="form-control" id="validationCustom01" value="Mark" name="cardID" required />
-								  	<label for="validationCustom01" class="form-label">Card ID</label>
-									<div class="valid-feedback">Valid!</div>
+								  	<input type="text" class="form-control" id="validationCustom01" value="Mark" name="empID" required />
+								  	<label for="validationCustom01" class="form-label">Employee ID</label>
+									<div class="valid-feedback">Valid ID</div>
+									<div class="invalid-feedback">Enter a valid ID</div>
 								</div>
 							</div>
 							<div class="col-md-4">
 								<div class="form-outline">
-							  		<input type="text" class="form-control" id="validationCustom02" value="Otto" name="name" required />
+							  		<input type="text" class="form-control" id="validationCustom02" value="Otto" name="empName" required />
 							  		<label for="validationCustom02" class="form-label">Name</label>
-							  		<div class="valid-feedback">Looks good!</div>
+							  		<div class="valid-feedback">Valid Name</div>
+							  		<div class="invalid-feedback">Enter a valid Name</div>
 								</div>
 							</div>
 							<div class="col-md-4">
 								<div class="input-group form-outline">
-							  		<input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" name="nrc" required />
+							  		<input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" name="empNRC" required />
 							  		<label for="validationCustomUsername" class="form-label">NRC</label>
-							  		<div class="invalid-feedback">Please choose a username.</div>
+							  		<div class="valid-feedback">Valid NRC</div>
+							  		<div class="invalid-feedback">Enter a valid NRC</div>
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-outline">
-							  		<input type="text" class="form-control" id="validationCustom03" name="position" required />
+							  		<input type="number" class="form-control" id="validationCustom03" name="empPositionID" required />
 							  		<label for="validationCustom03" class="form-label">Position</label>
 							  		<div class="invalid-feedback">Please provide a valid city.</div>
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-outline">
-							  		<input type="text" class="form-control" id="validationCustom04" name="dept" required />
+							  		<input type="text" class="form-control" id="validationCustom04" name="empDeptID" required />
 							  		<label for="validationCustom04" class="form-label">Department</label>
 							  		<div class="invalid-feedback">Please provide a valid zip.</div>
 								</div>
 							</div>
 							<div class="col-md-6">
 								<div class="form-outline">
-							  		<input type="text" class="form-control" id="validationCustom05" name="joinDate" required />
+							  		<input type="text" class="form-control" id="validationCustom05" name="empJoinDate" required />
 							  		<label for="validationCustom05" class="form-label">Join Date</label>
 							  	<div class="invalid-feedback">Please provide a valid zip.</div>
 								</div>
@@ -67,8 +81,48 @@
 							</div>
 						</form>
 		  			</div>
+		  			<div class="col-md-12 container px-5 py-3 mx-5 mt-3 card">
+		  				<!-- <div class="container"> -->
+					  		<?php
+					  			// if (isset($_SESSION['ReadMessage'])) {
+					  			// 	echo $_SESSION['ReadMessage'];
+					  			// 	unset($_SESSION['ReadMessage']);
+					  			// }
+					  		?>
+					  	<!-- </div> -->
+				  		<table class="table table-striped text-center">
+				  			<tr>
+				  				<thead colspan="3">
+				  					<h4>Employee Records</h4>
+				  					<br>
+				  				</thead>
+				  			</tr>
+				  			<tr>
+				  				<th>EmpID</th>
+				  				<th>EmpName</th>
+				  				<th>EmpJoinDate</th>
+				  			</tr>
+				  			<?php
+				  				include 'backend2.php';
+				  				$rows = empRead();
+				  				$i = 0;
+				  				while($i<$rows->ReadCount){
+				  					$tmp = 'row'.$i++;
+				  			?>
+				  			<tr>
+				  				<td><?php echo $rows->{$tmp}->empID; ?></td>
+				  				<td><?php echo $rows->{$tmp}->empName; ?></td>
+				  				<td><?php echo $rows->{$tmp}->empJoinDate; ?></td>
+				  			</tr>
+				  			<?php
+				  				}
+				  				echo "<script>console.log('made it here');</script>";
+				  			?>
+				  		</table>
+				  	</div>
 		  		</div>
 		  	</div>
+
 		</main>
 		<!--Main layout-->
 
